bc15814a61f8a5fff4549755492fc81d1c7e7cb6
rjv9645 <rjv9645@rit.edu>
2014-11-10 15:37:44 -0500

    Initial Asp.NET MVC Scaffolding.

---
+++ DiscoveryCenter/DiscoveryCenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiscoveryCenter.Startup))]
+namespace DiscoveryCenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
