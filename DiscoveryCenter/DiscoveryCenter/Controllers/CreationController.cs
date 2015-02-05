656c2b7a8411cf1d783864c6633d11b93cafdb72
rjv9645 <rjv9645@rit.edu>
2015-02-04 19:14:58 -0500

    Added - Scaffolding for initial Survey Creation / Edit

---
+++ DiscoveryCenter/DiscoveryCenter/Controllers/CreationController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DiscoveryCenter.Models;
+
+namespace DiscoveryCenter.Controllers
+{
+    public class CreationController : Controller
+    {
+        private SurveyContext db = new SurveyContext();
+
+        // GET: Creation
+        public ActionResult Index()
+        {
+            return View(db.Surveys.ToList());
+        }
+
+        // GET: Creation/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // GET: Creation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Creation/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,CreateDate")] Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Surveys.Add(survey);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(survey);
+        }
+
+        // GET: Creation/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // POST: Creation/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,CreateDate")] Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(survey).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(survey);
+        }
+
+        // GET: Creation/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // POST: Creation/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Survey survey = db.Surveys.Find(id);
+            db.Surveys.Remove(survey);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
